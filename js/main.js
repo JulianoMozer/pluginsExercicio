6f6ab6ead2edab026a6bec68e980f83d1ae78de5
juliano <mozerlogistica@gmail.com>
2024-06-26 19:35:06 -0300

    Atualização do formulário para validação de CPF, telefone e CEP

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-$(document).ready(function() {
-    $('#telefone').mask('(00) 00000-0000', {
-        placeholder: '(DDD) 12345-6789'
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
     });
 
-    $('#cpf').mask('000.000.000-00', {
-        placeholder: '123.456.789-00'
-    });
+    // Aplicando máscaras aos campos
+    $('#telefone').mask('(00) 00000-0000', { placeholder: '(DDD) 12345-6789' });
+    $('#cpf').mask('000.000.000-00', { placeholder: '123.456.789-00' });
+    $('#cep').mask('00000-000', { placeholder: '01234-567' });
 
-    $('#cep').mask('00000-000', {
-        placeholder: '012345-678'
-    });
-
-    $('form').validate({
+    // Validando o formulário
+    $('#form').validate({
         rules: {
             nome: {
                 required: true
@@ -21,24 +20,38 @@ $(document).ready(function() {
                 email: true
             },
             telefone: {
-                required: true
+                required: true,
+                minlength: 15 // Tamanho da máscara (15 caracteres)
+            },
+            cpf: {
+                required: true,
+                minlength: 14 // Tamanho da máscara (14 caracteres)
             },
             endereco: {
                 required: true
             },
             cep: {
-                required: true
-            },
-            cpf: {
-                required: true
+                required: true,
+                minlength: 9 // Tamanho da máscara (9 caracteres)
             }
         },
-        submitHandler: function(form) {
+        messages: {
+            telefone: {
+                minlength: "Por favor, digite um telefone válido"
+            },
+            cpf: {
+                minlength: "Por favor, digite um CPF válido"
+            },
+            cep: {
+                minlength: "Por favor, digite um CEP válido"
+            }
+        },
+        submitHandler: function (form) {
             alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
             form.reset();
         },
-        invalidHandler: function(form, validator) {
-            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha os campos corretamente para prosseguir com a compra!");
         }
     });
 });
